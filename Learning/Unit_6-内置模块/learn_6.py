6108b031389584e668639e5e8584ed71e8ce7c3e
WangDooo <wangbc1993@163.com>
2019-07-02 22:08:37 +0800

    Update learn_6.py

--- Learning/Unit_6-内置模块/learn_6.py
+++ Learning/Unit_6-内置模块/learn_6.py
@@ -63,36 +63,107 @@ import random
 # 44. 用copyreg实现可靠的pickle操作
 #----------------------------------------------------------------
 # 彼此不信任的人或程序之间，如果要进行通信，就应该使用JSON这样的格式
+
+# import pickle
+# import copyreg
+
+# class GameState(object):
+# 	def __init__(self):
+# 		self.level = 0
+# 		self.lives = 4
+
+# state = GameState()
+# state.level += 1 # beat a level
+# state.lives -= 1 # lose a live
+
+# state_path = './tmp/game_state.bin'
+# with open(state_path, 'wb') as f:
+# 	pickle.dump(state, f)
+
+# with open(state_path, 'rb') as f:
+# 	state_after = pickle.load(f)
+# print(state_after.__dict__)
+
+# # 借用内置的copyreg模块，用copyreg模块注册一些函数，Python对象的序列化，将由这些函数来责对
+# # 为缺失的属性提供默认值
+
+# # 1.重新定义GameState类的构造器
+# class GameState(object):
+# 	def __init__(self, level=0, lives=4, points=0, magic=5): # 增加 magic属性
+# 		self.level = level
+# 		self.lives = lives
+# 		self.points = points
+# 		self.magic = magic
+
+
+# # 定义辅助函数，接受GameState对象，并将其转换为一个包含参数的元组，以便提供给copyreg模块
+# def pickle_game_state(game_state):
+# 	kwargs = game_state.__dict__ # dict是用来存储对象属性的一个字典，其键为属性名，值为属性的值
+# 	return unpickle_game_state,(kwargs,)
+
+# def unpickle_game_state(kwargs):
+# 	return GameState(**kwargs)
+
+# # 用copyreg模块来注册pickle_game_state函数
+# copyreg.pickle(GameState, pickle_game_state)
+
+# # 序列化和反序列化操作 和原来一样
+# state = GameState()
+# state.points += 1000
+# serialized = pickle.dumps(state)
+# state_after = pickle.loads(serialized)
+# print(state_after.__dict__)
+
+# # 2. 用版本号来管理类	删除了原有类中的参数
+# def pickle_game_state(game_state):
+# 	kwargs = game_state.__dict__
+# 	kwargs['version'] = 2
+# 	return unpickle_game_state, (kwargs,)
+
+# def unpickle_game_state(kwargs):
+# 	version = kwargs.pop('version', 1)
+# 	if version == 1:
+# 		kwargs.pop('lives')
+# 	return GameState(**kwargs)
+
+# # 3. 固定的引入路径		类的名称进行修改
+
 #----------------------------------------------------------------
-import pickle
 
-class GameState(object):
-	def __init__(self):
-		self.level = 0
-		self.lives = 4
-
-state = GameState()
-state.level += 1 # beat a level
-state.lives -= 1 # lose a live
-
-state_path = './tmp/game_state.bin'
-with open(state_path, 'wb') as f:
-	pickle.dump(state, f)
-
-with open(state_path, 'rb') as f:
-	state_after = pickle.load(f)
-print(state_after.__dict__)
-
-#================================================================
-# 
-#----------------------------------------------------------------
-
-#----------------------------------------------------------------
 
 
 #================================================================
 # 45. 应该用datetime模块来处理本地时间，而不是用time模块
 #----------------------------------------------------------------
+# # 1. time模块
+# from time import localtime, strftime
+# import time 
+
+# now = time.time()
+# local_tuple = localtime(now)
+# time_format = '%Y-%m-%d %H:%M:%S'
+# time_str = strftime(time_format, local_tuple)
+# print(time_str)
+
+# # strptime函数 解析包含时间信息的字符串
+# # mktime函数 将本地时间转换为UNIX时间戳
+# from time import mktime, strptime
+
+# time_tuple = strptime(time_str, time_format)
+# utc_now = mktime(time_tuple)
+# print(utc_now)
+# print(time.time())
+
+# # 2. datetime模块
+# from datetime import datetime, timezone
+# import pytz
+
+# now = datetime(2019, 5, 30, 10, 45, 55)
+# now_utc = now.replace(tzinfo=timezone.utc)
+# now_local = now_utc.astimezone()
+# print(now_local)
+
+# pacific = pytz.timezone('US/Pacific')
 
 #----------------------------------------------------------------
 
